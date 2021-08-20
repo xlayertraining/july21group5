faef774491deb42e96558c40f0fc1175e6042235
Soumik Debnath <soumik@xlayer.in>
2021-08-20 15:34:00 +0530

    Signed-off-by: Soumik Debnath <soumik@xlayer.in>
    Material and Flex added.

--- portal/job-nest/src/app/app.module.ts
+++ portal/job-nest/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
