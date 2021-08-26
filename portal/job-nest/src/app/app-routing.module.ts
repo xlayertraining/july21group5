4daaeee1a287d682c8992edfe6deb39714e8c59c
Manisha Roy <manisharoy7005@gmail.com>
2021-08-26 14:23:56 +0530

    Home,Login,Signup is completed.

--- portal/job-nest/src/app/app-routing.module.ts
+++ portal/job-nest/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  { path: 'home',           component: HomeComponent},
+  { path: 'login',          component: LoginComponent},
+  { path: 'signup',         component: SignupComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+    ],
 })
 export class AppRoutingModule { }
--- portal/job-nest/src/app/app.module.ts
+++ portal/job-nest/src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Router, Routes, RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component'
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+const routes: Routes = [
+  {
+    path: 'sign_in',
+    component: LoginComponent,
+  },
+  // {
+    // path: 'table/grievance-info-dialog',
+    // component: GrievanceInfoDialogComponent
+    // }
+];
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
