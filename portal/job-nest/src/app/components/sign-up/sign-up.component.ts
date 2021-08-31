3fe950c1a9d521f1814bacbec027b322bcb262b6
Manisha Roy <manisharoy7005@gmail.com>
2021-08-31 20:10:00 +0530

    sign-in

--- portal/job-nest/src/app/components/sign-up/sign-up.component.ts
+++ portal/job-nest/src/app/components/sign-up/sign-up.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
+  
 })
 export class SignUpComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
