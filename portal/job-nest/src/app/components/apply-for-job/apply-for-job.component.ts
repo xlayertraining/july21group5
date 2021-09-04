f6d3614ed6b94a5e397cd983f751ea81e6b1b7e5
Soumik Debnath <dnsoumik.org@gmail.com>
2021-09-04 14:22:13 +0530

    04-09-21 Session updates.

--- portal/job-nest/src/app/components/apply-for-job/apply-for-job.component.ts
+++ portal/job-nest/src/app/components/apply-for-job/apply-for-job.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NetworkService } from 'src/app/service/network.service';
 
 @Component({
   selector: 'app-apply-for-job',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplyForJobComponent implements OnInit {
 
-  constructor() { }
+  applyJobsList: any[] = [
+    {
+      value: 1, 
+      viewValue: 'Work from home'
+    },
+    {
+      value: 2, 
+      viewValue: 'BPO Job'
+    }
+  ];
+
+  jobListArray: any[] = [];
+
+  constructor(private networkService: NetworkService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getJobsList();
+  }
+
+  getJobsList(): void {
+    this.networkService.getJobs().subscribe( success => {
+      console.log(success);
+     if (success.status) {
+       this.jobListArray = success.result;
+     } 
+    }, error => {});
+  }
+
+  goHome(): void {
+    this.router.navigate(['home']);
   }
 
 }
